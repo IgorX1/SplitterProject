1ef4acccc75fba031178c2913040f1942115df66
Igor Konobas <kois9911@gmail.com>
2018-10-10 21:04:32 +0300

    Added some groups

--- src/test/java/com/devx/SplitterNG.java
+++ src/test/java/com/devx/SplitterNG.java
@@ -1,11 +1,20 @@
 package com.devx;
 import org.testng.Assert;
 import org.testng.annotations.*;
+
+import java.util.List;
+
 import static org.testng.Assert.*;
 
 public class SplitterNG {
 
     private Splitter splitter;
+
+    @BeforeGroups(groups = {"unitLetterOperations",
+    "testSplitMethod"})
+    public void setUpInGroup(){
+        splitter = new Splitter();
+    }
 
     @BeforeTest
     public void setUp(){
@@ -24,7 +33,8 @@ public class SplitterNG {
         };
     }
 
-    @Test(dataProvider = "getWrongLetter")
+    @Test(dataProvider = "getWrongLetter",
+    groups = {"unitLetterOperations"})
     public void isLetter_notLetter_false(char c){
         assertFalse(splitter.isLetter(c));
     }
@@ -46,15 +56,35 @@ public class SplitterNG {
         };
     }
 
-    @Test(dataProvider = "getCorrectLetter")
+    @Test(dataProvider = "getCorrectLetter",
+    enabled = true,
+    groups = {"unitLetterOperations"})
     public void isLetter_realLetter_true(char c){
         assertTrue(splitter.isLetter(c));
     }
 
-    @Test(expectedExceptions = Exception.class)
+    @Test(expectedExceptions = Exception.class,
+    groups = {"testSplitMethod"})
     public void split_wrongInput_exception(){
         Splitter splitter1 = new Splitter(null);
         splitter1.split();
     }
 
+    @DataProvider
+    public Object[][] getTextSets(){
+        return new Object[][]{
+            {"123x --don't {{self-control", 3},
+            {"aaa_aaa-vv1cc zza", 4},
+            {"", 0},
+            {"oneword__12",1}
+        };
+    }
+
+    @Test(dataProvider = "getTextSets",
+    groups = {"testSplitMethod"})
+    public void split_fillList_correctNumberOfWords(String text, int words){
+        splitter.setText(text);
+        List res = splitter.split();
+        assertEquals(res.size(), words);
+    }
 }
